e116b6a2691ac11ced0baecc034db794e252fbdb
Zheka Chakhov <zhekachakhov@mail.ru>
2023-08-19 15:05:10 +0300

    set scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./index.css";
+import "./index.scss";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <></>;
+	return <>fjdkasjfaldk</>;
 }
 
 export default App;
